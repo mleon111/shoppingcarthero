6042813b16f8f996205195592f4f93e11c7ccbec
max leone <maxleone@maxs-MacBook-Pro.local>
2016-09-01 03:15:56 -0400

    Admin Shoppingcart

--- shoppingcart/models.py
+++ shoppingcart/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+	item_id = models.IntegerField(default=0)
+	item_name = models.CharField(max_length=200)
+	item_price = models.IntegerField(default=0)
+	item_quantity = models.IntegerField(default=0)
+	def __str__(self):
+		return self.item_name
