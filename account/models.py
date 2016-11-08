d459de4ab4ab83a990a38206c3512ac41dbffe93
max leone <maxleone@maxs-MacBook-Pro.local>
2016-11-08 13:22:05 -0500

    added default profile level

--- account/models.py
+++ account/models.py
@@ -6,7 +6,7 @@ class Profile(models.Model):
     date_of_birth = models.DateField(blank=True, null=True)
     photo = models.ImageField(upload_to='users/%Y/%m/%d', 
                               blank=True)
-    level = models.IntegerField()
+    level = models.IntegerField(default=1)
     date_coupon_used = models.DateTimeField(auto_now=True)
     
     def __str__(self):
