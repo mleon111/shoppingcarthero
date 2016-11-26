bb7c4e0eb143f313af59a5d9f9ac125844b5076a
Winicius Siqueira <winicius@gmail.com>
2016-11-26 16:09:06 -0500

    Serving Static Files debug false
    Serving Static Files

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import url
 
-from django.conf.urls.static import static
 from . import views
 handler404 = views.error404
 handler500 = views.error404
--- shoppingcarthero/urls.py
+++ shoppingcarthero/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+from django.views.static import serve
 
 handler404 = views.error404
 handler500 = views.error404
@@ -20,5 +21,4 @@ urlpatterns = [
 if settings.DEBUG is True:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 else:
-	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+	urlpatterns += [url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT,})]
